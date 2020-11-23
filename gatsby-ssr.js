f2442124b6c28beff7ce6e91b465f2292a6deab8
Rael Bernardino <bernardino1207@gmail.com>
2020-11-23 13:00:30 -0800

    Add wrapRootElement() for RecoilRoot

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+
+export const wrapRootElement = ({ element, props }) => (
+    <RecoilRoot {...props}>{element}</RecoilRoot>
+)
