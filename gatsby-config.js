5d582fbad7283a83456cb497cdce0eb1bb1e41f0
Rael Bernardino <bernardino1207@gmail.com>
2020-12-08 20:12:04 -0800

    icon and website title

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ const { BLOCKS, MARKS, INLINES } = require('@contentful/rich-text-types');
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rael Bernardino`,
+    description: `Rael Bernardino, a frontend developer, ui/ux designer, freelance.`,
+    author: `Rael Bernardino`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -45,7 +45,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/raelicon.png`, // This path is relative to the root of the site.
       },
     },
     {
