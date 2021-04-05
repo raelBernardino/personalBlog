46a38123e455b07d6bf9c2fe508999ee0a43fc64
Rael Bernardino <bernardino1207@gmail.com>
2021-04-05 14:10:43 -0700

    Add missing useEffect dependency

--- src/pages/analog.js
+++ src/pages/analog.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Container, Typography, Button, Nav, NavDrawer, NavBar, Footer } from '../components/atoms';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
